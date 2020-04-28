e5ce034aac077680755236f52e47989f76a80690
timoharju <timoharju17@gmail.com>
2020-04-28 15:43:08 +0300

    Added admin module and firebase config

---
+++ vitsiapp-functions/functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAy4lon0rqoVdIOYKu3tSxTkhQy4QhRN7c",
+    authDomain: "vitsi-app-80584.firebaseapp.com",
+    databaseURL: "https://vitsi-app-80584.firebaseio.com",
+    projectId: "vitsi-app-80584",
+    storageBucket: "vitsi-app-80584.appspot.com",
+    messagingSenderId: "1032607648146",
+    appId: "1:1032607648146:web:065d871a8f8a2e3ad3594b",
+    measurementId: "G-Q40GY22277"
+};
